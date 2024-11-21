7afaa268334ec9afafbba2f24dfd4dd048ce692d
josue marchena <josuemarchena0730@gmail.com>
2024-11-21 12:48:17 -0500

    añanido archivo ignore, refactorizando codigo en el directorio Memoizacion - 01.js. Cerrando issue #1

--- Memoizacion/01.js
+++ Memoizacion/01.js
@@ -24,43 +24,6 @@ console.log(factorial(5)); // Calcula y almacena
 console.log(factorial(5)); // Recupera desde cache
 
 
-
-// FIBONACCIO AVANZADO
-
-// function memoizedFibonacci() {
-//     const cache = {}; // Almacén para resultados calculados
-
-//     return function fibonacci(n) {
-//         if (n in cache) {
-//             console.log(`Usando cache para ${n}`);
-//             return cache[n];
-//         }
-
-//         if (n === 0) return 0;
-//         if (n === 1) return 1;
-
-//         const result = fibonacci(n - 1) + fibonacci(n - 2);
-//         cache[n] = result; // Guarda el resultado en cache
-//         return result;
-//     };
-// }
-
-// const fibonacci = memoizedFibonacci();
-
-// console.log(fibonacci(10)); // Calcula y almacena
-// console.log(fibonacci(10)); // Recupera desde cache
-
-
-/*
-¿Cuándo usar memoización?
-La memoización es ideal para problemas que cumplen con estas características:
-
-Subproblemas repetitivos: La solución involucra el mismo cálculo varias veces.
-Funciones puras: El resultado de la función depende únicamente de sus argumentos (sin efectos secundarios).
-Recursividad: Especialmente útil en algoritmos como Fibonacci, factorial, o búsqueda de caminos en grafos.
- */
-
-
 /*
 Optimización de algoritmos recursivos:
 
