3e569aeee0208686a934b31f47c7876c40b003a9
josue marchena <josuemarchena0730@gmail.com>
2024-11-21 16:00:39 -0500

    Fix #2 - Refactorizando main.py

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 name = 'joshua'
 age = 18
 
-print('hola ' + name + ', tu edad es ' + str(age))
+print(f'Hola {name}, tu edad es {age}') # saludo mas eficiente!
+
+
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    
+    def __str__(self):
+        return f'Hola {self.name}, tu edad es {self.age}'
+
+p = Person('joshua', 18)
+print(p)
