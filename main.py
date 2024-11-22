de6085b73d3f699c0da4e193efdabb55ea3a3ede
josue marchena <josuemarchena0730@gmail.com>
2024-11-21 22:24:40 -0500

    Fix #3 - Agregando nuevas funcionalidades y propiedades a main.py

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 name = 'joshua'
 age = 18
 
-print(f'Hola {name}, tu edad es {age}') # saludo mas eficiente!
+# print(f'Hola {name}, tu edad es {age}') # saludo mas eficiente!
 
 
 class Person:
-    def __init__(self, name, age):
+    def __init__(self, name, age, job):
         self.name = name
         self.age = age
+        self.job = job
     
-    def __str__(self):
-        return f'Hola {self.name}, tu edad es {self.age}'
+    def __sal__(self):
+        print(f'Hola {self.name}, tu edad es {self.age} y trabajas como {self.job}')
 
-p = Person('joshua', 18)
-print(p)
+p = Person('joshua', 18, 'developer')
+print(p.__sal__())
+
+# comentario agregado al final de la funcion
