3d2fb8761cbcf8375e923ede147ecc898517ee72
josue marchena <josuemarchena0730@gmail.com>
2024-11-28 21:25:06 -0500

    Matrix con arrays en py

--- main.py
+++ main.py
@@ -1,20 +1,7 @@
-name = 'joshua'
-age = 18
-job = 'developer'
-# print(f'Hola {name}, tu edad es {age}') # saludo mas eficiente!
+# Matrix 
 
-
-class Person:
-    def __init__(self, name, age, job):
-        self.name = name
-        self.age = age
-        self.job = job
-    
-    def __sal__(self):
-        print(f'Hola {self.name}, tu edad es {self.age} y trabajas como {self.job}')
-        return 0;
-
-p = Person(name, age, job)
-print(p.__sal__())
-
-# comentario agregado al final de la funcion
+for i in range(3):
+    array = []
+    for j in range(3):
+        array.append('*')
+    print(array)
